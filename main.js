e599cceb02aad1061fa83f992d06db16a18f9826
Popescu Iulian <popescu.iulian@protonmail.com>
2020-01-29 23:04:09 +0200

    new things learned

---
+++ main.js
@@ -0,0 +1,16 @@
+// const nume = document.querySelector('.nume');
+// const numar = document.querySelector('.numar');
+// const listaNume = document.querySelector('.lista-nume');
+
+// function adaugaInLista() {
+//   const x = `
+//   <li>
+//     <span>${nume.value}</span><span>${numar.value}</span><button>edit</button
+//     ><button>delete</button>
+//   </li>`;
+
+//   listaNume.insertAdjacentHTML('beforeend', x);
+
+//   nume.value = '';
+//   numar.value = '';
+// }
