28dda9135443247d311e70ff45840d90e37c1fae
Popescu-Iulian <popescu.iulian@protonmail.com>
2020-02-01 15:22:07 +0200

    simple layout

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// const nume = document.querySelector('.nume');
-// const numar = document.querySelector('.numar');
-// const listaNume = document.querySelector('.lista-nume');
+const nume = document.querySelector('.nume');
+const numar = document.querySelector('.numar');
+const listaNume = document.querySelector('.lista-nume');
 
-// function adaugaInLista() {
-//   const x = `
-//   <li>
-//     <span>${nume.value}</span><span>${numar.value}</span><button>edit</button
-//     ><button>delete</button>
-//   </li>`;
+function adaugaInLista() {
+	const x = `
+  <li>
+    <span>${nume.value}</span><span>${numar.value}</span><button>edit</button
+    ><button>delete</button>
+  </li>`;
 
-//   listaNume.insertAdjacentHTML('beforeend', x);
+	listaNume.insertAdjacentHTML('beforeend', x);
 
-//   nume.value = '';
-//   numar.value = '';
-// }
+	nume.value = '';
+	numar.value = '';
+}
